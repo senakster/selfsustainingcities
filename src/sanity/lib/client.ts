4e8142967d4303551f05d511f4809a668ce2a32c
Emil Nakayama <senakster@gmail.com>
2025-10-12 14:01:29 +0200

    logging

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,7 +1,8 @@
+'use server'
 import 'server-only'
 
+
 import { createClient } from "next-sanity";
-import isDraftMode from "./helpers/isDraftMode"
 import { apiVersion, dataset, projectId } from "../env";
 type TSanityFetchProps = {
   isPreview?: boolean
@@ -10,17 +11,16 @@ type TSanityFetchProps = {
   params?: object
   tags?: string[]
 }
+const token = process.env.SANITY_STUDIO_EDITOR_TOKEN;
 
 export async function sanityClient<T>({
   isPreview,
   query,
   params = {},
   tags = [],
-}: TSanityFetchProps): Promise<T> {
-  'use server'
-  const token = process.env.SANITY_STUDIO_EDITOR_TOKEN;
-  
-    return createClient({
+}: TSanityFetchProps): Promise<T> { 
+  console.log({token, dataset, apiVersion, isPreview })    
+    const response = createClient({
       projectId,
       dataset,
       apiVersion,
@@ -43,5 +43,7 @@ export async function sanityClient<T>({
       tags,
     },
   })
+  console.log('response', response.then(console.log))
+  return response
 }
 
