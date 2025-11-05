582304d5ac1bbe189b2becaeee62d87f8fb2e76e
Emil Nakayama <senakster@gmail.com>
2025-11-05 01:22:22 +0100

    IconColor

--- src/assets/icons/LanguageIcon.tsx
+++ src/assets/icons/LanguageIcon.tsx
@@ -1,7 +1,7 @@
 export const LanguageIcon = ({className, ...rest}: {className?: string, rest?: React.SVGProps<SVGSVGElement>}) => {
   return (<svg
   xmlns="http://www.w3.org/2000/svg"
-  fill="#000000"
+  fill="currentColor"
   className={className}
   {...rest}
   viewBox="0 0 512 512">
